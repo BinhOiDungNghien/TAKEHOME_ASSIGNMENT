bcbec0a5ba60c98a1a9399ef64bcd4df055b9ee2
BinhOiDungNghien <lengocbinh09062003@gmail.com>
2026-03-03 10:58:05 +0700

    feat: add basic UI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.api import api_router
 from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Connect all routes to the main app instance
